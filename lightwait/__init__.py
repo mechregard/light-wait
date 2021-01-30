4b362bb0eef8e51f2cbbbda3d4db0e2eccd0e252
dlange <dave.lange@seal-software.com>
2021-01-30 15:15:32 +0100

    release 0.0.2 includes keeping date of imported files

--- lightwait/__init__.py
+++ lightwait/__init__.py
@@ -3,6 +3,6 @@
 from .lightwait import LightWait
 from .exception import LightwaitException
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'dlange <dlange@mechregard.com>'
 __all__ = []
