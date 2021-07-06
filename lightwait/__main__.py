20b0652dec229575b167e8d652e936b32889c165
David Lange <David@Melinas-MBP.lan>
2021-07-06 11:12:46 +0200

    support for bulk import/export, metadata edit

--- lightwait/__main__.py
+++ lightwait/__main__.py
@@ -7,13 +7,16 @@ def main():
     """CLI Generate blog content"""
 
     parser = argparse.ArgumentParser(description="Generate blog post")
-    parser.add_argument("-c", "--command", help="import or  generate", required=True)
+    parser.add_argument("-c", "--command", help="import, bulk or generate", required=True)
 
     # import options
     parser.add_argument("-f", "--file", help="import markdown file path")
     parser.add_argument("-n", "--name", type=str, help="import short unique name")
     parser.add_argument("-d", "--description", type=str, help="import description")
     parser.add_argument("-t", "--tags", type=str, help="import tags list")
+
+    # bulk options
+    parser.add_argument("-s", "--src", help="import markdown src directory path")
 
     # generate options
     parser.add_argument("-o", "--output", help="generate output directory")
@@ -32,6 +35,18 @@ def main():
         except:
             print("General error:", sys.exc_info()[0])
 
+    elif args.command == 'bulk':
+        try:
+            if args.src is not None:
+                LightWait().import_dir(args.src)
+                print("Bulk imported markdown:  ", args.src)
+            else:
+                parser.print_help()
+        except LightwaitException as msg:
+            print("Bulk import failed:", msg)
+        except:
+            print("General error:", sys.exc_info()[0])
+
     elif args.command == 'generate':
         try:
             if args.output is not None:
@@ -41,6 +56,17 @@ def main():
                 parser.print_help()
         except LightwaitException as msg:
             print("Generate failed:", msg)
+        except:
+            print("General error:", sys.exc_info()[0])
+    elif args.command == 'export':
+        try:
+            if args.output is not None:
+                LightWait().export(args.output)
+                print("Exported content to: ", args.output)
+            else:
+                parser.print_help()
+        except LightwaitException as msg:
+            print("export failed:", msg)
         except:
             print("General error:", sys.exc_info()[0])
 
